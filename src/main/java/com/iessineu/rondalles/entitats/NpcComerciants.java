03872a9c68dfefebf785643c46da7d11c7941129
Sergi Jordà Martorell <sergi5501@iessineu.net>
2026-06-03 18:44:05 +0200

    msps

--- src/main/java/com/iessineu/rondalles/entitats/NpcComerciants.java
+++ src/main/java/com/iessineu/rondalles/entitats/NpcComerciants.java
@@ -7,7 +7,7 @@ public class NpcComerciants extends Entitat {
     private final int pis;
     private boolean enigmaResult = false;
 
-    private static final String[][] ENIGMES = {
+    private static final String[][] ENIGMES = { //enigma, resposta
         {"Tenc ales però no vol, tenc cua però no és animal. Què sóc?", "una fletxa"},
         {"Com més gran em fas, menys pots veure. Què sóc?",             "la foscor"},
         {"Estic sempre davant teu però no es pot veure. Què sóc?",      "el future"},
@@ -15,12 +15,12 @@ public class NpcComerciants extends Entitat {
         {"Tenc boca però no puc parlar, tenc llit però no puc dormir. Què sóc?", "un riu"}
     };
 
-    public NpcComerciants(int x, int y, int pis) {
+    public NpcComerciants(int x, int y, int pis) { //pis entre 1 i 5
         super(x, y, 'N');
         this.pis = Math.max(1, Math.min(5, pis));
     }
 
-    public String getEnigma()  { return ENIGMES[pis - 1][0]; }
+    public String getEnigma()  { return ENIGMES[pis - 1][0]; } //pis-1 perquè el pis va de 1 a 5 però els índexs van de 0 a 4
 
     public boolean comprovaSolucio(String resposta) {
         enigmaResult = resposta.trim().equalsIgnoreCase(ENIGMES[pis - 1][1]);
