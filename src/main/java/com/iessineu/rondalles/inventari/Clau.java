e9ca9ca404d1ecb0dba7385c5ae1365bbef11754
jaumesa-iesineu <jaume7225@iessineu.net>
2026-06-05 15:39:42 +0200

    afegeix classe clau

---
+++ src/main/java/com/iessineu/rondalles/inventari/Clau.java
@@ -0,0 +1,33 @@
+package com.iessineu.rondalles.inventari;
+
+import com.googlecode.lanterna.TextColor;
+import com.iessineu.rondalles.entitats.Jugador;
+
+public class Clau extends Item {
+
+    private int planta;
+    private String id;
+
+    public Clau(String id, String nom, int pes, char simbol, int planta) {
+        super(nom, pes, simbol);
+        this.id = id;
+        this.planta = planta;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getPlanta() {
+        return planta;
+    }
+
+    @Override
+    public void aplicaEfecte(Jugador jugador) {
+    }
+
+    @Override
+    public TextColor getColor() {
+        return new TextColor.RGB(255, 215, 0);
+    }
+}
